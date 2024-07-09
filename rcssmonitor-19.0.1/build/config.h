069730efb63af88e583ab958a030e67fb2798d3d
lcabriq99 <lcabriq1999@gmail.com>
2024-07-09 14:27:13 +0200

    Initial commit

---
+++ rcssmonitor-19.0.1/build/config.h
@@ -0,0 +1,10 @@
+
+#define PACKAGE_NAME "rcssmonitor"
+
+#define VERSION "19.0.1"
+
+#define HAVE_LIBZ
+
+/* #undef HAVE_WINDOWS_H */
+
+#define HAVE_ARPA_INET_H
---
+++ rcssmonitor-19.0.1/build/rcss/rcg/CMakeFiles/rcssrcg.dir/rcssrcg_autogen/mocs_compilation.cpp.o.d
@@ -0,0 +1,3 @@
+rcss/rcg/CMakeFiles/rcssrcg.dir/rcssrcg_autogen/mocs_compilation.cpp.o: \
+ /home/lcabriq/oesia/TB/rcssmonitor-19.0.1/build/rcss/rcg/rcssrcg_autogen/mocs_compilation.cpp \
+ /usr/include/stdc-predef.h
